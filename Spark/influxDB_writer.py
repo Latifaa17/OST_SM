c91503839fc174894425da0bd76fa3fdc1f0dad7
latifaa17 <latifaadhibi1@gmail.com>
2022-11-20 22:01:58 +0100

    remove unused library

--- Spark/influxDB_writer.py
+++ Spark/influxDB_writer.py
@@ -1,5 +1,4 @@
 from pyspark.sql import Row
-from sklearn.preprocessing import MinMaxScaler
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
